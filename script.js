50aecd3dfccb4f809e96a2405d69901ddcee2131
dave-bayas <dglbayas@pcu.edu.ph>
2026-08-26 16:25:40 +0800

    update

--- script.js
+++ script.js
@@ -28,6 +28,3 @@ function openImage(imageSrc) {
 function closeImage() {
     document.getElementById("imageModal").style.display = "none";
 }
-function openPDF(pdfPath) {
-    window.open(pdfPath, "_blank");
-}
