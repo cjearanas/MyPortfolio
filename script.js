74e1c2d72525b8f5176e7ad5785d61ad89b67282
dave-bayas <dglbayas@pcu.edu.ph>
2026-08-26 15:16:46 +0800

    update

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
     });
 
 });
+function openImage(imageSrc) {
+    const modal = document.getElementById("imageModal");
+    const fullImage = document.getElementById("fullImage");
+
+    fullImage.src = imageSrc;
+    modal.style.display = "flex";
+}
+
+function closeImage() {
+    document.getElementById("imageModal").style.display = "none";
+}
